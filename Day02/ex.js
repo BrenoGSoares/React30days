8535213e2b26a906dd810c30808da930e2a2b852
Breno <breno.givas@icloud.com>
2023-10-24 16:23:59 -0300

    done: day02

---
+++ Day02/ex.js
@@ -0,0 +1,78 @@
+// 1 - What is React?
+// React é uma biblioteca Open Soruce do JavaScript desenvolvida pelo facebook, atual Meta.
+
+// 2 - What is a library?
+// São coleções de de recursos, como métodos e funções, prontos para utilizar em códigos.
+
+// 3 - What is a single page application?
+// É a única página que terá qualquer aplicação React.
+
+// 4 - What is a component ?
+// Componentes são blocos de construção reutilizáveis que permitem dividir a interface do usuário em partes independentes, um exemplo são os commponentes JSX
+
+// 5 - What is the latest version of React?
+// Atualmente é a versão 18.2.0
+
+// 6 - What is DOM?
+// Document Object Model é a representação dos dados dos objetos que formam um documento Web
+
+// 7 - What is React Virtual DOM?
+// VDOM é um conceito de programação onde a representação "virtual" da IU é mantida na memória e sincronizada com o DOM "Real".
+// No React nós não alteramos diretamente o DOM, e sim indicamos a partir do VDOM as alterações.
+
+// 8 - What does a web application or a website(composed of) have?
+// Podemos dividir em duas partes, Frontend e Backend. O front básicamente será a construção visual e o Backend a lógica da aplicação.
+
+// 1 - Why did you chose to use react?
+// Além de ser uma biblioteca extremamente popular, o react tem características agrádaveis como ser rápido, modular, escalável...
+
+// 2 - What measures do you use to know popularity ?
+// Aqui a popularidade foi baseada na quantidade de Forks e Stars nos respectivos repositórios
+
+// 3 - What is more popular, React or Vue ?
+// Diria que é o React, principalmente pelo apoio do Facebook
+
+// 4 - What is an HTML element?
+// Um elemento HTML é um objeto dentro de um documento web
+
+// 5 - How to write a self closing HTML element?
+// Abrindo os elementos que são nulos, ou seja, que não pode ter nada dentro deles pois não é possível fechar. Um exemplo é <meta charser=”UTF-8>, é uma tag que apenas abre.
+
+// 6 - What is an HTML attribute? Write some of them
+// Atributo HTML é qualquer coisa que modifica ou identifica aquele elemento, como por exemplo: Id, Class, Style...
+
+// 7 - What is JSX?
+// Significa JavaScript XML, básicamente permite escrever elementos HTML com código JS.
+
+// 8 - What is babel?
+// Babel é uma biblioteca que transpila JSX para JavaScript puro e JavaScript mais recente para versões mais antigas.
+
+// 9 - What is a transpiler?
+// Converte JSX para JavaScript
+
+// 1 - What is a JSX element?
+// Um elemento JSX é semelhante ao elemento HTML mas é representado como um elemento JS
+
+// 2 - Write your name in a JSX element and store it in a name variable
+// Feito no Index
+
+// 3 - Write a JSX element which displays your full name, country, title, gender, email, phone number. Use h1 for the name and p for the rest of the information and store it in a user variable
+// Feito no Index
+
+// 4 - Write a footer JSX element
+// feito no index
+
+// 1 - Create a style object for the main JSX
+// feito no index
+
+// 2 - Create a style object for the footer and app JSX
+// feito no index
+
+// 3 - Add more styles to the JSX elements
+// feito no index
+
+// 1 - Apply different styles to your JSX elements
+// feito no index
+
+// 1 - Practice how to make JSX element and injecting dynamic data(string, number, boolean, array, object)
+// feito no index
