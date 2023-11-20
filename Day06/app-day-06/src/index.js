5e03fd55040a9f5647fe0b66ae8d5801171aa77e
Breno <breno.givas@icloud.com>
2023-11-20 16:19:16 -0300

    feat: day 06

--- Day06/app-day-06/src/index.js
+++ Day06/app-day-06/src/index.js
@@ -3,91 +3,107 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 // JSX element, header
-const Header = () => {
+const Header = (props) => {
   return (
     <header>
       <div className="title">
         <h3>30 Days Of React</h3>
       </div>
-      <h4 className="subtitle">Number Generator</h4>
+      <h4 className="subtitle">{props.subtitle}</h4>
     </header>
   )
 }
 
 const Main = (props) => {
-  const primeNumber = (num) => {
-    if (num === 0 || num === 1) {
-      return false
+  if (props.numbers) {
+    const primeNumber = (num) => {
+      if (num === 0 || num === 1) {
+        return false
+      }
+      for (let divisor = 2; divisor < num; divisor++)
+        if (num % divisor === 0) return false
+      return true
     }
-    for (let divisor = 2; divisor < num; divisor++)
-      if (num % divisor === 0) return false
-    return true
+    const numberBlock = props.numbers.map((num) => {
+      if (primeNumber(num)) {
+        return (
+          <h1
+            style={{ backgroundColor: 'rgb(250, 81, 81)' }}
+            className="block-container"
+            key={num}
+          >
+            {num}
+          </h1>
+        )
+      } else if (num % 2 === 0) {
+        return (
+          <h1
+            style={{ backgroundColor: 'rgb(94, 196, 111)' }}
+            className="block-container"
+            key={num}
+          >
+            {num}
+          </h1>
+        )
+      } else {
+        return (
+          <h1
+            style={{ backgroundColor: 'rgb(250, 213, 81)' }}
+            className="block-container"
+            key={num}
+          >
+            {num}
+          </h1>
+        )
+      }
+    })
+    return numberBlock
+  } else {
+    const colorBlock = props.color.map((color) => {
+      return (
+        <h1
+          style={{ backgroundColor: color }}
+          className="block-container block-container-hexa"
+          key={color}
+        >
+          {color}
+        </h1>
+      )
+    })
+    return colorBlock
   }
-  const numberBlock = props.numbers.map((num) => {
-    if (primeNumber(num)) {
-      return (
-        <h1
-          style={{ backgroundColor: 'rgb(250, 81, 81)' }}
-          className="block-container"
-          key={num}
-        >
-          {num}
-        </h1>
-      )
-    } else if (num % 2 === 0) {
-      return (
-        <h1
-          style={{ backgroundColor: 'rgb(94, 196, 111)' }}
-          className="block-container"
-          key={num}
-        >
-          {num}
-        </h1>
-      )
-    } else {
-      return (
-        <h1
-          style={{ backgroundColor: 'rgb(250, 213, 81)' }}
-          className="block-container"
-          key={num}
-        >
-          {num}
-        </h1>
-      )
-    }
-  })
-  return numberBlock
 }
 
-// const hexaColor = () => {
-//   let str = '0123456789abcdef'
-//   let color = ''
-//   for (let i = 0; i < 6; i++) {
-//     let index = Math.floor(Math.random() * str.length)
-//     color += str[index]
-//   }
-//   return '#' + color
-// }
-
 // JSX element, app
+const subtitle = ['Number Generator', 'Hexadecimal Colors']
 const App = () => {
-  const numbersList = []
-  for (let i = 0; i <= 31; i++) {
-    numbersList.push(i)
+  const hexaColor = () => {
+    let str = '0123456789abcdef'
+    let color = ''
+    for (let i = 0; i < 6; i++) {
+      let index = Math.floor(Math.random() * str.length)
+      color += str[index]
+    }
+    return '#' + color
   }
 
-  // const colorList = [
-  //   hexaColor(),
-  //   hexaColor(),
-  //   hexaColor(),
-  //   hexaColor(),
-  //   hexaColor(),
-  // ]
+  const numbersList = []
+  const colorList = []
+
+  for (let i = 0; i <= 31; i++) {
+    numbersList.push(i)
+    colorList.push(hexaColor())
+  }
+
   return (
     <div className="app">
-      <Header />
+      <Header subtitle={subtitle[0]} />
       <div className="container">
         <Main numbers={numbersList} />
+      </div>
+      <Header subtitle={subtitle[1]} />
+      <div className="container">
+        <Main color={colorList} />
       </div>
     </div>
   )
